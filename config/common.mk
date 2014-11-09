453862ca676eddad0a9ac9294265c08b649774f4
noname55804 <marlin2201@gmail.com>
2014-11-09 01:56:19 +0100

    delete those for now

--- config/common.mk
+++ config/common.mk
@@ -70,14 +70,11 @@ PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/etc/chapps/Greenify.apk:system/app/Greenify.apk \
     vendor/liquid/prebuilt/common/etc/chapps/TitaniumBackup.apk:system/app/TitaniumBackup.apk \
     vendor/liquid/prebuilt/common/etc/chapps/SDMaid.apk:system/app/SDMaid.apk \
-   #vendor/liquid/prebuilt/common/etc/chapps/UpdateMe.apk:system/app/UpdateMe.apk \
     vendor/liquid/prebuilt/common/etc/trickster/Trickster.apk:system/app/TricksterMod.apk \
     vendor/liquid/prebuilt/common/etc/chapps/BetterBatteryStats.apk:system/priv-app/BetterBatteryStats.apk \
     vendor/liquid/prebuilt/common/etc/themestore/ThemeStore.apk:system/app/ThemeStore.apk \
     vendor/liquid/prebuilt/common/etc/chapps/OperaMax.apk:system/hackapp/OperaMax.apk \
-    vendor/liquid/prebuilt/common/etc/chapps/FolderMount.apk:system/hackapp/FolderMount.apk \
-   #vendor/liquid/prebuilt/common/etc/ota/update_me.xml:system/update_me.xml \
-	
+    vendor/liquid/prebuilt/common/etc/chapps/FolderMount.apk:system/hackapp/FolderMount.apk
 	
 # superuser
 SUPERUSER_EMBEDDED := true
